41f02de3dca525137323cc902357f775097f1e96
Christian Voigt <wwevo@users.noreply.github.com>
2019-01-10 21:30:46 +0100

    added get_ip function to determine the public ip of the bot

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
+import socket
 from webserver import Webserver
 from time import sleep
 
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+
 webserver_options = {
-    "host": "127.0.0.1",
+    "host": get_ip(),
     "port": 5000,
     "Flask_secret_key": "thisissecret",
     "SocketIO_debug": True
