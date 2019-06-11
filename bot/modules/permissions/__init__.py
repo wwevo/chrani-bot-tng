5666ebb2aa06551162a78df90268e37bfd2d8236
Christian Voigt <wwevo@users.noreply.github.com>
2019-06-11 22:42:42 +0200

    added permissions widget will display all available actions to users with appropriate level

--- bot/modules/permissions/__init__.py
+++ bot/modules/permissions/__init__.py
@@ -11,10 +11,12 @@ class Permissions(Module):
         })
         setattr(self, "required_modules", [
             'module_dom',
-            'module_players'
+            'module_players',
+            'module_webserver'
         ])
         self.next_cycle = 0
         self.run_observer_interval = 5
+        self.all_available_actions_dict = {}
         Module.__init__(self)
 
     @staticmethod
@@ -28,8 +30,18 @@ class Permissions(Module):
 
     def start(self):
         self.set_permission_hooks()
+        self.all_available_actions_dict = self.get_all_available_actions()
         Module.start(self)
     # endregion
+
+    @staticmethod
+    def get_all_available_actions():
+        all_available_actions_dict = {}
+        for loaded_module_identifier, loaded_module in loaded_modules_dict.items():
+            if len(loaded_module.available_actions_dict) >= 1:
+                all_available_actions_dict[loaded_module_identifier] = loaded_module.available_actions_dict
+
+        return all_available_actions_dict
 
     def trigger_action_with_permission(self, module, event_data, dispatchers_id=None):
         """ Manually for now, this will be handled by a permissions widget. """
@@ -38,7 +50,8 @@ class Permissions(Module):
         if any([
                 event_data[0] == "toggle_player_table_widget_view",
                 event_data[0] == "toggle_whitelist_widget_view",
-                event_data[0] == "toggle_webserver_status_view"
+                event_data[0] == "toggle_webserver_status_view",
+                event_data[0] == "toggle_permissions_view"
         ]):
             if event_data[1]["action"] == "show_options":
                 if int(module.dom.data.get("module_players", {}).get("admins", {}).get(dispatchers_id, 2000)) >= 2:
---
+++ bot/modules/permissions/widgets/permissions_widget.py
@@ -0,0 +1,98 @@
+from bot import loaded_modules_dict
+from os import path, pardir
+
+module_name = path.basename(path.normpath(path.join(path.abspath(__file__), pardir, pardir)))
+widget_name = path.basename(path.abspath(__file__))[:-3]
+
+
+def select_view(*args, **kwargs):
+    module = args[0]
+    dispatchers_steamid = kwargs.get('dispatchers_steamid', None)
+
+    current_view = module.dom.data.get(module.get_module_identifier(), {}).get("visibility", {}).get(dispatchers_steamid, {}).get(
+        "current_view", "frontend"
+    )
+
+    if current_view == "options":
+        options_view(module, dispatchers_steamid)
+    else:
+        frontend_view(module, dispatchers_steamid)
+
+
+def frontend_view(module, dispatchers_steamid=None):
+    template_frontend = module.templates.get_template('permissions_widget/view_frontend.html')
+
+    template_options_toggle = module.templates.get_template('permissions_widget/control_switch_view.html')
+    template_options_toggle_view = module.templates.get_template(
+        'permissions_widget/control_switch_options_view.html'
+    )
+
+    current_view = module.dom.data.get(module.get_module_identifier(), {}).get("visibility", {}).get(dispatchers_steamid, {}).get(
+        "current_view", "frontend"
+    )
+
+    data_to_emit = template_frontend.render(
+        options_toggle=template_options_toggle.render(
+            control_switch_options_view=template_options_toggle_view.render(
+                steamid=dispatchers_steamid,
+                options_view_toggle=(True if current_view == "frontend" else False)
+            )
+        )
+    )
+
+    module.webserver.send_data_to_client(
+        event_data=data_to_emit,
+        data_type="widget_content",
+        clients=[dispatchers_steamid],
+        target_element={
+            "id": "permissions_widget",
+            "type": "table",
+            "selector": "body > main > div"
+        }
+    )
+
+
+def options_view(module, dispatchers_steamid=None):
+    template_frontend = module.templates.get_template('permissions_widget/view_options.html')
+
+    template_options_toggle = module.templates.get_template('permissions_widget/control_switch_view.html')
+    template_options_toggle_view = module.templates.get_template(
+        'permissions_widget/control_switch_options_view.html'
+    )
+
+    current_view = module.dom.data.get(module.get_module_identifier(), {}).get("visibility", {}).get(dispatchers_steamid, {}).get(
+        "current_view", "frontend"
+    )
+
+    data_to_emit = template_frontend.render(
+        options_toggle=template_options_toggle.render(
+            control_switch_options_view=template_options_toggle_view.render(
+                steamid=dispatchers_steamid,
+                options_view_toggle=(True if current_view == "frontend" else False)
+            )
+        ),
+        widget_options=module.options,
+        available_actions=module.all_available_actions_dict
+    )
+
+    module.webserver.send_data_to_client(
+        event_data=data_to_emit,
+        data_type="widget_content",
+        clients=[dispatchers_steamid],
+        target_element={
+            "id": "permissions_widget",
+            "type": "table",
+            "selector": "body > main > div"
+        }
+    )
+
+
+widget_meta = {
+    "description": "shows permissions and stuff",
+    "main_widget": select_view,
+    "handlers": {
+        "module_permissions/visibility/%steamid%/current_view": select_view
+    }
+}
+
+loaded_modules_dict["module_" + module_name].register_widget(widget_name, widget_meta)
