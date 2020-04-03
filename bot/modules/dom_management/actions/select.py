7f72ff2a53ad92ff001ddb421c7f2e91738f200c
wwevo <evoisat@gmail.com>
2020-04-03 09:28:30 +0200

    played around with the select action, added some info, probably made it better overall. experimenting with error messages. picked the wrong module for that as it rarely errors out at all :)

--- bot/modules/dom_management/actions/select.py
+++ bot/modules/dom_management/actions/select.py
@@ -9,14 +9,23 @@ def main_function(module, event_data, dispatchers_steamid):
     action = event_data[1].get("action", None)
     target_module = event_data[1].get("target_module", None)
     event_data[1]["action_identifier"] = action_name
+    event_data[1]["fail_reason"] = []
 
     dom_element_select_root = event_data[1].get("dom_element_select_root", ["selected_by"])
     dom_element_origin = event_data[1].get("dom_element_origin", None)
     dom_element_owner = event_data[1].get("dom_element_owner", None)
     dom_element_identifier = event_data[1].get("dom_element_identifier", None)
 
-    if action is not None:
-        if action == "select_dom_element" or action == "deselect_dom_element":
+    if all([
+        action is not None,
+        dom_element_origin is not None,
+        dom_element_owner is not None,
+        dom_element_identifier is not None
+    ]):
+        if action in [  # only proceed with known commands
+            "select_dom_element",
+            "deselect_dom_element"
+        ]:
             general_root = [target_module, "elements", dom_element_origin, dom_element_owner]
             full_root = general_root + dom_element_select_root
 
@@ -37,7 +46,7 @@ def main_function(module, event_data, dispatchers_steamid):
             max_discovered_level = 0
             selected_by_dict = {}
             for path_fragment in reversed(dom_element_select_root):
-                if max_discovered_level == 0:
+                if max_discovered_level == 0:  # the first level will get metadata
                     selected_by_dict = {
                         path_fragment: selected_by_dict_element,
                         "dataset": dom_element_origin,
@@ -50,9 +59,12 @@ def main_function(module, event_data, dispatchers_steamid):
                     }
                 max_discovered_level += 1
 
-            """ we do a # + max_discovered_level so we won't trigger actions associated at lower levels
-            the 3 in this example translates to "%target_module%/elements/%element_origin%/%dom_element_owner"
-            otherwise we'd possibly trigger widget-updates whenever we select something
+            """ we do a min_callback_level at # + max_discovered_level so we won't trigger actions associated at lower
+            levels. the 3 in this case translates to
+                "%target_module%(0)/elements(1)/%element_origin%(2)/%dom_element_owner(3)"
+                
+            This way we will only trigger on changes from dom_element_owner and deeper, which is what we want.
+            I'm sure with better design this could be done automatically, feel free to contribute if you know how :)
             """
             module.dom.data.upsert({
                 target_module: {
@@ -67,8 +79,18 @@ def main_function(module, event_data, dispatchers_steamid):
             module.callback_success(callback_success, module, event_data, dispatchers_steamid)
             return
 
+        else:
+            event_data[1]["fail_reason"].append("unknown action")
+    else:
+        event_data[1]["fail_reason"].append("required options not set")
+
     module.callback_fail(callback_fail, module, event_data, dispatchers_steamid)
     return
+
+
+""" these will not be called directly. Always call the modules_callback and that will call this local function:
+module.callback_fail(callback_fail, module, event_data, dispatchers_steamid)
+"""
 
 
 def callback_success(module, event_data, dispatchers_steamid, match=None):
@@ -80,7 +102,7 @@ def callback_fail(module, event_data, dispatchers_steamid):
 
 
 action_meta = {
-    "description": "tools to help managing dom elements in the webinterface",
+    "description": "handles selecting or deselecting an element in the dom for further actions",
     "main_function": main_function,
     "callback_success": callback_success,
     "callback_fail": callback_fail,
